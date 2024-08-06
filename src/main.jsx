352c54d54fbe2171b79c4b479347bb6791ec497f
JesusOsama <jesusv2911@gmail.com>
2024-08-06 18:37:47 -0500

    Manejo de Listas y Botones

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import {ContadorApp} from './ContadorApp'
+import {ListadoApp} from './ListadoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ContadorApp/>
+    <ListadoApp/>
   </React.StrictMode>,
 )
  
